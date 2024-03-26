bac6ca09f0bab62f01180ef04b79ea0478f173b0
Jade McAlister <mcalister.vjade@gmail.com>
2024-03-26 13:01:18 -0400

    Read data from file

--- Main.py
+++ Main.py
@@ -1,2 +1,10 @@
+import csv
 import pandas as pd
 import numpy as np
+data = pd.read_csv(
+   'C:\\Users\\veron\\PycharmProjects\\E-Commerce-Lifetime-Value-Prediction\\E-commerce Customer Behavior - Sheet1.csv')
+
+to_drop = ['Membership Type', 'Average Rating', 'Discount Applied', 'Days Since Last Purchase']
+
+
+print(data)
