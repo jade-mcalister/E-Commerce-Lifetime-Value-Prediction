32784184d38b1cbd42876e28f17412ca5398c30c
Jade McAlister <mcalister.vjade@gmail.com>
2024-03-26 15:48:04 -0400

    Drops unnecessary columns

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import csv
 import pandas as pd
 import numpy as np
+
+#reads csv file
 data = pd.read_csv(
    'C:\\Users\\veron\\PycharmProjects\\E-Commerce-Lifetime-Value-Prediction\\E-commerce Customer Behavior - Sheet1.csv')
 
-to_drop = ['Membership Type', 'Average Rating', 'Discount Applied', 'Days Since Last Purchase']
-
-
+#drops unnecessary columns
+to_drop = ['Membership Type', 'Average Rating', 'Discount Applied', 'Satisfaction Level', 'Days Since Last Purchase']
+data = data.drop(to_drop, axis=1)
 print(data)
