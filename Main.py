aa71421b87f01b521076033a932aef6350f0963e
Jade McAlister <mcalister.vjade@gmail.com>
2024-03-26 15:49:13 -0400

    Drops unnecessary columns (edited)

--- Main.py
+++ Main.py
@@ -7,6 +7,6 @@ data = pd.read_csv(
    'C:\\Users\\veron\\PycharmProjects\\E-Commerce-Lifetime-Value-Prediction\\E-commerce Customer Behavior - Sheet1.csv')
 
 #drops unnecessary columns
-to_drop = ['Membership Type', 'Average Rating', 'Discount Applied', 'Satisfaction Level', 'Days Since Last Purchase']
+to_drop = ['Membership Type', 'Average Rating', 'Discount Applied', 'Satisfaction Level']
 data = data.drop(to_drop, axis=1)
 print(data)
